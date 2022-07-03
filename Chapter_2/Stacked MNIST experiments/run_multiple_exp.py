4ba9b920483482d0c1981e14ff17f9f3044487a4
Sanaz Mohammad Jafari <43353182+sanazMj@users.noreply.github.com>
2022-07-03 13:55:33 -0400

    Update run_multiple_exp.py

--- Chapter_2/Stacked MNIST experiments/run_multiple_exp.py
+++ Chapter_2/Stacked MNIST experiments/run_multiple_exp.py
@@ -6,26 +6,6 @@ Num = 5
 num_epoch = 30
 #
 from GAN import ex
-# for j in range( Num):
-#     print('GAN',j, num_epoch)
-#
-#     torch.cuda.empty_cache()
-#     ex.run(config_updates={ 'num_epochs': num_epoch,  'pac_dim':1})
-# for j in range( Num):
-#     print('PacGAN',j, num_epoch)
-#
-#     torch.cuda.empty_cache()
-#     ex.run(config_updates={ 'num_epochs': num_epoch,  'pac_dim':4})
-#
-
-
-from VARNET import ex
-# # from VARNET_with_labels import ex
-# for j in range(Num):
-#     print('varnet',j,30 )
-#
-#     torch.cuda.empty_cache()
-#     ex.run(config_updates={ 'num_epochs': 30,  'pac_dim':1, 'Model_structure': 'FF'})
 
 for j in range(1, Num):
     print('Pacvarnet', j, 30)
@@ -44,12 +24,3 @@ for j in range(Num):
 
     torch.cuda.empty_cache()
     ex.run(config_updates={'num_epochs': 30, 'pac_dim': 4, 'Model_structure': 'Convolutional'})
-#
-# #
-#
-# from VARNET import ex
-# for j in range(Num):
-#     print('pacvarnet',j, 20)
-#
-#     torch.cuda.empty_cache()
-#     ex.run(config_updates={ 'num_epochs': 20,  'pac_dim':4})
